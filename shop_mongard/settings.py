9eacd78147bb6dd7d627de7dd17502bf4b3713ee
Arash <arashkarimi1999@gmail.com>
2024-04-11 22:44:13 +0330

    add base, messages, navbar templates

--- shop_mongard/settings.py
+++ shop_mongard/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "shop_mongard.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
